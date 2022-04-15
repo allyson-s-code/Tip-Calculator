a18cc4c61c7d218b9f28efa56e19246ad9dd2b9c
allyson_s <allysonsmithstudio@gmail.com>
2022-04-15 09:24:25 -0400

    Added basic tip calculator functions

---
+++ js/script.js
@@ -0,0 +1,64 @@
+const billInput = document.getElementById("bill");
+const tipButtons = document.querySelectorAll("button");
+const customTip = document.getElementById("custom-btn");
+const numPeople = document.getElementById("num-people");
+const alert = document.querySelector(".alert-text");
+const tipAmountPP = document.querySelector(".tip-amount span");
+const totalAmount = document.querySelector(".total span");
+
+//Global variables
+let bill, selectedTip, tipInput, people;
+
+//input
+function input() {
+  bill = Number(billInput.value);
+  tipInput = selectedTip || Number(customTip.value);
+  people = Number(numPeople.value);
+}
+
+//update tip and total amount
+function updateAmount() {
+  input();
+  updateTip();
+  updateTotal();
+}
+
+//calcuate tip and update tip span
+function updateTip() {
+  if (people !== 0) {
+    tip = (bill * tipInput) / 100 / people;
+    tipAmountPP.innerText = `${tip.toFixed(2)}`;
+  }
+}
+
+//calculate total and update total span
+function updateTotal() {
+  if (people !== 0) {
+    total = (bill + (bill * tipInput) / 100) / people;
+    totalAmount.innerText = `${total.toFixed(2)}`;
+  }
+}
+
+//grab what was entered in the bill input
+billInput.addEventListener("input", function (e) {
+  updateAmount();
+});
+
+//grab what was selected from tip buttons
+for (const button of tipButtons) {
+  button.addEventListener("click", function (e) {
+    selectedTip = e.target.value;
+    updateAmount();
+  });
+}
+
+//grab custom tip
+customTip.addEventListener("input", function (e) {
+  selectedTip = null;
+  updateAmount();
+});
+
+//grab number of people input
+numPeople.addEventListener("input", function (e) {
+  updateAmount();
+});
